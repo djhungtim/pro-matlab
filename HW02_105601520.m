d73e84fb507c05f89cd7ed71484b9c1edd30b3ef
Hong Hua Ting <djhungtim@gmail.com>
2020-03-12 16:58:39 +0800

    HW2 finish

--- HW02_105601520.m
+++ HW02_105601520.m
@@ -16,3 +16,33 @@ disp(num_of_workspace); % disp number of elements
 %%
 
 % problem 2. 
+
+% ihead, jhead, khead are unit vector and orthogonal to each other
+ihead = [1 0 0];
+jhead = [0 1 0];
+khead = [0 0 1];
+
+% create three vector named A,B,C
+A = 2*ihead + 3*jhead + 1*khead;
+B = 4*ihead + 6*jhead + 2*khead;
+C = 3*ihead + 4*jhead + 2*khead;
+
+% calculate vector size of A,B
+size_of_A = sqrt(sum(A.^2));
+size_of_B = sqrt(sum(B.^2));
+
+% calculate unit vector of A,B
+unit_vector_A = A/size_of_A;
+unit_vector_B = B/size_of_B;
+
+% calculate inner product of vector A and C
+inner_product_of_vector_AC = A(1)*C(1) + A(2)*C(2) + A(3)*C(3); 
+
+% calculate angle between A,B and A,C in rad.
+angle_AB = acos((A(1)*B(1) + A(2)*B(2) + A(3)*B(3))/(size_of_A*size_of_B));
+angle_AC = acos((inner_product_of_vector_AC) / (size_of_A*sqrt(sum(C.^2))));
+
+% calculate outer product of A,C
+outer_product_of_vector_AC = [(A(2)*C(3) - A(3)*C(2)), (A(3)*C(1) - A(1)*C(3)), (A(1)*C(2) - A(2)*C(1))];
+
+
