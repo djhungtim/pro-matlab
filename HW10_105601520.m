5d5bca601b4eadea975059c5aac17764ee9a5353
Hong Hua Ting <djhungtim@gmail.com>
2020-06-11 15:50:27 +0800

    refresh

--- HW10_105601520.m
+++ HW10_105601520.m
@@ -1,192 +1,58 @@
 
+
 clc
-clear 
+clear
+i = [];
 
+
+for idx = 1:8
 load planets.mat
 
-composition = composition(1,1);
-density = density(1,1);
-diameter = diameter(1,1);
-distance = distance(1,1);
-eccentricity = eccentricity(1,1);
-escape = escape(1,1);
-gravity = gravity(1,1);
-inclination = inclination(1,1);
-mass = mass(1,1);
-moons = moons(1,1);
-name = name(1,1);
-period = period(1,1);
-rings = rings(1,1);
-rotation = rotation(1,1);
-temperature = temperature(1,1);
-velocity = velocity(1,1);
-save('Mercury1.mat','composition','density',...
+composition = composition(1,idx);
+density = density(1,idx);
+diameter = diameter(1,idx);
+distance = distance(1,idx);
+eccentricity = eccentricity(1,idx);
+escape = escape(1,idx);
+gravity = gravity(1,idx);
+inclination = inclination(1,idx);
+mass = mass(1,idx);
+moons = moons(1,idx);
+period = period(1,idx);
+rings = rings(1,idx);
+rotation = rotation(1,idx);
+temperature = temperature(1,idx);
+velocity = velocity(1,idx);
+
+name = name(1,idx);
+newname = lower(name);
+
+filename = newname + string(idx);
+save(filename,'composition','density',...
 'diameter','distance','eccentricity','escape','gravity','inclination',...
 'mass','moons','name','period','rings','rotation','temperature','velocity')
-%%
-load planets.mat
-
-composition = composition(1,2);
-density = density(1,2);
-diameter = diameter(1,2);
-distance = distance(1,2);
-eccentricity = eccentricity(1,2);
-escape = escape(1,2);
-gravity = gravity(1,2);
-inclination = inclination(1,2);
-mass = mass(1,2);
-moons = moons(1,2);
-name = name(1,2);
-period = period(1,2);
-rings = rings(1,2);
-rotation = rotation(1,2);
-temperature = temperature(1,2);
-velocity = velocity(1,2);
-save('venus2.mat','composition','density',...
-'diameter','distance','eccentricity','escape','gravity','inclination',...
-'mass','moons','name','period','rings','rotation','temperature','velocity')
-
-%%
-
-load planets.mat
-
-composition = composition(1,3);
-density = density(1,3);
-diameter = diameter(1,3);
-distance = distance(1,3);
-eccentricity = eccentricity(1,3);
-escape = escape(1,3);
-gravity = gravity(1,3);
-inclination = inclination(1,3);
-mass = mass(1,3);
-moons = moons(1,3);
-name = name(1,3);
-period = period(1,3);
-rings = rings(1,3);
-rotation = rotation(1,3);
-temperature = temperature(1,3);
-velocity = velocity(1,3);
-save('earth3.mat','composition','density',...
-'diameter','distance','eccentricity','escape','gravity','inclination',...
-'mass','moons','name','period','rings','rotation','temperature','velocity')
-%%
+end
 
 
-load planets.mat
-
-composition = composition(1,4);
-density = density(1,4);
-diameter = diameter(1,4);
-distance = distance(1,4);
-eccentricity = eccentricity(1,4);
-escape = escape(1,4);
-gravity = gravity(1,4);
-inclination = inclination(1,4);
-mass = mass(1,4);
-moons = moons(1,4);
-name = name(1,4);
-period = period(1,4);
-rings = rings(1,4);
-rotation = rotation(1,4);
-temperature = temperature(1,4);
-velocity = velocity(1,4);
-save('mars4.mat','composition','density',...
-'diameter','distance','eccentricity','escape','gravity','inclination',...
-'mass','moons','name','period','rings','rotation','temperature','velocity')
-%%
-
-
-load planets.mat
-
-composition = composition(1,5);
-density = density(1,5);
-diameter = diameter(1,5);
-distance = distance(1,5);
-eccentricity = eccentricity(1,5);
-escape = escape(1,5);
-gravity = gravity(1,5);
-inclination = inclination(1,5);
-mass = mass(1,5);
-moons = moons(1,5);
-name = name(1,5);
-period = period(1,5);
-rings = rings(1,5);
-rotation = rotation(1,5);
-temperature = temperature(1,5);
-velocity = velocity(1,5);
-save('jupiter5.mat','composition','density',...
-'diameter','distance','eccentricity','escape','gravity','inclination',...
-'mass','moons','name','period','rings','rotation','temperature','velocity')
 
 %%
 
 
-load planets.mat
-
-composition = composition(1,6);
-density = density(1,6);
-diameter = diameter(1,6);
-distance = distance(1,6);
-eccentricity = eccentricity(1,6);
-escape = escape(1,6);
-gravity = gravity(1,6);
-inclination = inclination(1,6);
-mass = mass(1,6);
-moons = moons(1,6);
-name = name(1,6);
-period = period(1,6);
-rings = rings(1,6);
-rotation = rotation(1,6);
-temperature = temperature(1,6);
-velocity = velocity(1,6);
-save('saturn6.mat','composition','density',...
-'diameter','distance','eccentricity','escape','gravity','inclination',...
-'mass','moons','name','period','rings','rotation','temperature','velocity')
-%%
+load ionosphere.mat
+rng('shuffle')
 
 
-load planets.mat
+rng(1);
+xtrain = randi([1 351], 1, 246);
+ytrain = xtrain;
 
-composition = composition(1,7);
-density = density(1,7);
-diameter = diameter(1,7);
-distance = distance(1,7);
-eccentricity = eccentricity(1,7);
-escape = escape(1,7);
-gravity = gravity(1,7);
-inclination = inclination(1,7);
-mass = mass(1,7);
-moons = moons(1,7);
-name = name(1,7);
-period = period(1,7);
-rings = rings(1,7);
-rotation = rotation(1,7);
-temperature = temperature(1,7);
-velocity = velocity(1,7);
-save('uranus7.mat','composition','density',...
-'diameter','distance','eccentricity','escape','gravity','inclination',...
-'mass','moons','name','period','rings','rotation','temperature','velocity')
-%%
+rng(2);
+xtest = randi([1 351], 1, 105);
+ytest = xtest;
 
 
-load planets.mat
+X_train = X(xtrain, :);
+Y_train = Y(ytrain, :);
 
-composition = composition(1,8);
-density = density(1,8);
-diameter = diameter(1,8);
-distance = distance(1,8);
-eccentricity = eccentricity(1,8);
-escape = escape(1,8);
-gravity = gravity(1,8);
-inclination = inclination(1,8);
-mass = mass(1,8);
-moons = moons(1,8);
-name = name(1,8);
-period = period(1,8);
-rings = rings(1,8);
-rotation = rotation(1,8);
-temperature = temperature(1,8);
-velocity = velocity(1,8);
-save('neptune8.mat','composition','density',...
-'diameter','distance','eccentricity','escape','gravity','inclination',...
-'mass','moons','name','period','rings','rotation','temperature','velocity')
+X_test = X(xtest, :);
+Y_test = Y(ytest, :);
--- HW12_105601520.m
+++ HW12_105601520.m
@@ -1,19 +1,134 @@
 
-clc;
-clear;
+clc
+clear
+close
 
+%x:people y:jellies
+%7*x - 2 = y --> 7*x - y = 2
+%8*x + 5 = y --> 8*x - y = 5
+
+%left divide method
+A = [7 -1;8 -1];
+B = [2;5];
+X = A\B; %inv(A)*B
+check_ans = A*X;
+
+%Cramer's rule
+a = det([B, A(:,2)])/det(A);
+b = det([B, A(:,1)])/det(A);
+
+%Pseudo Inverse Matrix
+X1 = pinv(A)*B;
+
+%Eigenvalues Eigenvector
 A = [1 2;3 2];
-
-e = eig(A); %eigenvalues of matrix A £f
-
-[V,D] = eig(A); % Av = £fv  V = eigenvectors
+[V,D] = eig(A); %A*V = V*D.
+check = A*V == V*D;
 
 %%
-E = [-4 -2];
-F = [8 9;-1 -1];
-f = inv(F);
-%x:people y:jellies
-g = E*f;%[x y] = [2 20]
+% problem 3
+
+subplot(2,2,1)
+x=-4:0.1:4;
+rng(2);
+num=numel(x);
+y=2*x+randn(1,num);
+plot(x,y,'bo')
+
+xlim([-8 8]);
+ylim([-8 8]);
+axis square
+ax = gca;
+xticks([-6 -4 -2 2 4 6])
+xticklabels({'-6', '-4', '-2', '2', '4', '6'})
+yticks([-6 -4 -2 2 4 6])
+yticklabels({'-6', '-4', '-2', '2', '4', '6'})
+ax.XAxisLocation = 'origin';
+ax.YAxisLocation = 'origin';
+title('Original Coordinates')
+text(1,7,'Y')
+text(6,1,'X')
+box off
+
+%%
+subplot(2,2,2)
+
+A = [x;y]';
+[U,S,V] = svd(A);
+
+asin(V(1,1))
+acos(V(2,1))
+theta = asin(V(1,1));
+rotarr = []
+
+x=-4:0.1:4;
+rng(2);
+num=numel(x);
+y=2*x+randn(1,num);
+plot(x,y,'bo')
+
+xlim([-10 10]); 
+ylim([-8 8]);
+axis equal
+ax = gca;
+xticks([-8 -6 -4 -2 2 4 6 8])
+xticklabels({'-8', '-6', '-4', '-2', '2', '4', '6', '8'})
+yticks([-6 -4 -2 2 4 6])
+yticklabels({'-6', '-4', '-2', '2', '4', '6'})
+ax.XAxisLocation = 'origin';
+ax.YAxisLocation = 'origin';
+title('Findinf New Coordinates By Singular Value Decomposition')
+text(1,7,'Y')
+text(8,1,'X')
+
+hold on
+
+
+
+box off
+
+%%
+
+subplot(2,2,3)
+
+
+
+A = [x;y]';
+[U,S,V] = svd(A);
+
+x=-4:0.1:4;
+rng(2);
+num=numel(x);
+y=2*x+randn(1,num);
+% plot(x,y,'bo')
+
+xlim([-8 8]);
+ylim([-8 8]);
+axis square
+ax = gca;
+xticks([-6 -4 -2 2 4 6])
+xticklabels({'-6', '-4', '-2', '2', '4', '6'})
+yticks([-6 -4 -2 2 4 6])
+yticklabels({'-6', '-4', '-2', '2', '4', '6'})
+ax.XAxisLocation = 'origin';
+ax.YAxisLocation = 'origin';
+title('New Coordinates')
+text(1,7,'Y\prime')
+text(6,2,'X\prime')
+
+
+new_y = A*V(:,2);
+new_x = A*V(:,1);
+hold on
+plot(new_x,new_y,'bo')
+
+box off
+
+
+
+
+
+
 
 
 
